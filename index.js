32d94be56083dc68ddb931645b49ab95677b6892
ccahnha <me@parser.so>
2017-04-09 23:08:49 +0300

    Fixed escaping chars

--- index.js
+++ index.js
@@ -16,57 +16,65 @@ var MarkovChain = require('markovchain')
   , unlockDate={}
 
 tg.on('message', msg => {
-  var str = msg.text.split(' ').sort(function(a,b){a.length - b.length})
+  var str = msg.text.match(/((([a-z]|[а-я])|\d)|_)+/gi)
     , options = {reply_to_message_id : msg.message_id}
     , strId
     , re
 
-  console.log(Date() + ' ' + msg.from.username + ': ' + str)
+  console.log(Date() + msg.from.username + ': ' + str)
   unlockDate[msg.chat.id]
 
   if (!msg.text) {
     return
-  } 
+  } else if (str === null) {
+    return
+  }
   
+  str = str.sort(function(a,b){a.length - b.length})
+
+
 for (var i = 0; i < str.length; i++) {
-  re = RegExp(str[i]+'([a-z]|[а-я])*', 'ig')
-  if (keywords.match(re) !== null) {
+  re = RegExp(str[i]+'((([a-z]|[а-я])|\d)|_)+', 'ig')
+  if (keywords.match(re) !== null && str[i].length > 2) {
     strId = i
-    console.log('Keyword' + str[i] + 'detected')
+    console.log("keyword: " + s[i] )
     break
   }
 }
 
-  if (msg.chat.type == 'group' && strId === undefined) {
-    if (unlockDate[msg.chat.id] > msg.date) { 
-      return // Without such limitation this bot can quickly become annoying 
+  if (msg.chat.type !== "private") {
+    if (unlockDate[msg.chat.id] > msg.date && strId == undefined) {
+      return
     }
-    unlockDate[msg.chat.id] = msg.date + getRandomIntInclusive(1800,3600)
+    unlockDate[msg.chat.id] = msg.date + getRandomIntInclusive(1800,3600) 
   }
 
   var startWord = function(wordList) {
     var tmpList = Object.keys(wordList).filter(function(word) {
-      if (strId === undefined | strId === null) { // Runs when no keyword matched
+      if (strId === undefined | strId === null) {
         for (let i = str.length-1; i >= 0; i--) {
-          re = RegExp('(^|\s)'+str[i]+'([a-z]|[а-я])*', 'ig')
-          if (word.match(re)) { // If chain contains word from message, use it as start
+          re = RegExp('(^|\s)'+str[i]+'((([a-z]|[а-я])|\d)|_)+', 'ig')
+          if (word.match(re)) {
             return word
           }
         }
       }
-      re = RegExp('(^|\s)'+str[strId]+'([a-z]|[а-я])*', 'ig') 
-      if (word.match(re)) { // Same with keyword
+
+      re = RegExp('(^|\s)'+str[strId]+'((([a-z]|[а-я])|\d)|_)+', 'ig')
+      if (word.match(re)) {
         return word
       } 
     })
     return tmpList[~~(Math.random()*tmpList.length)]
-  }  
+  }
 
+try {
   let result = quotes.start(startWord).end().process()
   console.log(result)
   if (result !== 'undefined symbols') {
     tg.sendMessage(msg.chat.id, result, options)
   }
+} catch (e) {return}
 
 })
 
